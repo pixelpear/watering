9d0c15fe6504fbf269b49d2564a29a5e6f95ec56
Alena Bekrina <abekrina@gmail.com>
2018-03-02 14:18:33 +0100

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ToolbarAndroid,
+  Platform
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style={styles.statusbar} barStyle="light-content" />
+        <ToolbarAndroid
+          style={styles.toolbar}
+          titleColor="white"
+          title="Watering"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#87c0f2'
+  },
+  statusbar: {
+    height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight
+  },
+  toolbar: {
+    width: '100%',
+    height: 56,
+    backgroundColor: '#2d88d8'
+  }
+});
